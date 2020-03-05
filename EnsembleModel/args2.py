97fc8ddafcd9e9f2f2379c12855339607fec27bf
Guillaume N <gnervo@stanford.edu>
2020-03-04 22:25:06 -0800

    minor fix char embed

--- EnsembleModel/args2.py
+++ EnsembleModel/args2.py
@@ -59,7 +59,7 @@ def get_setup_args():
                         help='Max number of words in a question at test time')
     parser.add_argument('--char_dim',
                         type=int,
-                        default=200,
+                        default=64,
                         help='Size of char vectors (char-level embeddings)')
     parser.add_argument('--glove_dim',
                         type=int,
